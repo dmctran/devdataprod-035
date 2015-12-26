78b4de68dc4828114c3fdafb1ec29196d000c450
Dominic Tran <dmctran@gmail.com>
2015-12-26 12:31:14 +1100

    initial

---
+++ server.R
@@ -0,0 +1,88 @@
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  # display plots
+  output$doPlots <- renderPlot({
+    # parameters
+    n <- input$n
+    num_sim <- input$num_sim    
+    lambda <- input$lambda
+
+    # print params    
+    input_values <- data.frame(c(lambda, n, num_sim), 
+                               row.names = c("Lambda:", 
+                                             "Number of simulations:", "Sample size:"))    
+    colnames(input_values) <- ""
+    output$input_values <- renderPrint({input_values})
+    
+    # exponential distribution
+    exp_mean <- 1/lambda
+    exp_stdev <- 1/lambda
+    exp_var <- exp_stdev ^ 2
+    exp <- c(mean = exp_mean, var = exp_var, sd = exp_stdev)
+    
+    # normal distribution parameters
+    norm_mean <- exp_mean
+    norm_var <- exp_var / n
+    norm_stdev <- sqrt(norm_var)
+    theorical <- c(norm_mean, norm_var, norm_stdev)
+    
+    # sampling
+    set.seed(11)
+    data1 = NULL
+    for (i in 1 : num_sim) data1 = c(data1, mean(rexp(n, lambda)))
+    
+    sample_mean <- mean(data1)
+    sample_var <- var(data1)
+    sample_stdev <- sd(data1)
+    sample <- c(sample_mean, sample_var, sample_stdev)
+    
+    # summary of values
+    dist_values <- round(rbind("Exponential distribution" = exp, 
+                               "Theoretical Normal distribution" = theorical, 
+                               "Distribution of averages" = sample), 2)
+    output$dist_values <- renderPrint({dist_values})
+    
+    # plot
+    layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE),
+           widths=c(1,1), heights=c(5,4))
+    
+    # mean of sample
+    main_1 <- paste("Distribution of the mean of", n, "exponentials (")
+    main_params <- list(n = n, lambda = lambda)
+    hist(data1, breaks = 21, probability = TRUE, 
+         main = bquote("Distribution of the mean of" ~ .(main_params$n) 
+                       ~ "exponentials (" ~ lambda == .(main_params$lambda) ~ ")"),
+         xlab = NULL)
+    abline(v = norm_mean, lwd = 2, lty = 2, col = "blue")
+    abline(v = sample_mean, lty = 2, col = "red")
+    lines(norm_mean - norm_stdev, 0.02, col = "blue", type = "p", pch = 17)
+    lines(norm_mean + norm_stdev, 0.02, col = "blue", type = "p", pch = 17)
+    lines(sample_mean - sample_stdev, 0, col = "red", type = "p", pch = 16)
+    lines(sample_mean + sample_stdev, 0, col = "red", type = "p", pch = 16)
+    curve(dnorm(x, mean = norm_mean, sd = norm_stdev), 
+          col = "blue", lwd = 2, add = TRUE)
+    legend("topright", bty = "n", 
+           legend = c("Normal distribution",
+                      "Theoretical mean", "Sample mean", 
+                      "1 theoretical stdev", "1 sample stdev"), 
+           col = c("blue", "blue", "red", "blue", "red"), 
+           lty = c(1, 2, 2, NA, NA),
+           lwd = c(2, 2, 1, NA, NA),
+           pch = c(NA, NA, NA, 17, 16))
+    
+    # exponential
+    curve(dexp(x, rate = lambda), col = "darkblue", lwd = 2, from = 0, to = 30,
+          main = bquote("Underlying Exponential distribution (" 
+                        ~ lambda == .(main_params$lambda) ~ ")"),
+          xlab = NULL, y = "Density")
+    abline(v = exp_mean, lty = 2, col = "red")
+    mean_label <- paste("Mean", round(sample_mean, 2))
+    legend("right", bty = "n", legend = mean_label, col = "red", lty = 2)
+    
+    # qq
+    qqnorm(data1)
+    qqline(data1, col = "darkblue", lwd = 2)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Central Limit Theorem demonstration"),
+  
+  # Sidebar with a slider input for the number of bins
+  fluidRow(
+    column(3,
+      sliderInput("lambda", "Lambda:",
+                       min = 0.1, max = 5, value = 0.2, step = 0.1),
+      sliderInput("n", "Sample size (num averages):",
+                  min = 1, max = 50, value = 40),           
+      sliderInput("num_sim", "Number of simulations:",  
+                  min = 20, max = 5000, value = 100, step = 20),
+      submitButton("Submit")
+    ),
+    column(6, offset = 1,
+           h4("Submitted values"),
+           verbatimTextOutput("input_values"),
+           br(),
+           h4("Distributions parameters"),
+           verbatimTextOutput("dist_values")           
+    )
+  ),
+    
+  hr(),
+  plotOutput("doPlots")
+))
